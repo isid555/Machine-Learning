126013636fc8e7757057fca402d7a7eca106c68b
isid555 <r555sid@gmail.com>
2024-03-02 19:44:01 +0530

    first commit

---
+++ Module-1/notes.md
@@ -0,0 +1,41 @@
+# Machine Learning
+# Types of machine learning
+
+1. Supervised learning(learns from the labelled data set)
+2. Unsupervised learning(learns from the unlabelled data set)
+3. Reinforcement learning
+
+
+
+
+<hr>
+ Supervised 
+
+ - Classisfication - Predictiong about a class
+ - Regression - Predictiong values
+
+ <hr>
+ 
+ #### Unsupervised
+ - Clustering  - groups similar points
+ - Association - finds the realtion between data points
+
+ ### Unsupervised Learning Algorithms
+- K-Means Clustering
+- Hiearchial Clustering
+- Principal Component Analysis(PCA)
+- Apriori
+- Eclat
+
+
+<hr>
+
+### Deep Learning
+Deep learing uses Artificial neural networks to learn from the data
+
+
+
+
+
+
+
---
+++ Module-2/python basics.py
@@ -0,0 +1,51 @@
+hero1 , hero2 , hero3 = 'Ajith' , 'Dhanush' , 'Vijay'
+# # print(hero1,hero2  ,hero3)
+# print(hero1)
+# print(hero2)
+# print(hero3)
+
+
+x= y = z =5
+# print(x+y+z)
+
+# n1 = int(input("enter number"))
+# print(n1)
+
+# n2 =int( input("enter number"))
+# print(n2)
+# print(n1+n2)
+
+# print("hello" *0)
+
+### Slicing
+string  = "programming"
+# print(string[1:6:2])
+
+
+
+#       List ? Tuples ? Dictionaries
+
+# immutable data types 
+# int , str, bool ,float ,tuple
+
+# mutale data types
+# list 
+# set
+# dict
+
+my_list  =[1,2,3,4,5]
+# print(my_list)
+
+
+# dictionary
+my_dict = {'name':"Sid" , 'age':'76','address':'Business'}
+# print(my_dict)
+
+
+# if - elif -else  
+# elif is like else if in java
+
+
+
+
+
